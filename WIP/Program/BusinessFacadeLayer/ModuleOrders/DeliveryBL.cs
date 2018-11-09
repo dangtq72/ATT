b04ce6a119c2338a6cddd954072445b379de98c2
anhtuan nguyen <nguyenanhtuanxx1996@outlook.com>
2018-11-09 18:08:44 +0700

    Tuanna them delivery

---
+++ WIP/Program/BusinessFacadeLayer/ModuleOrders/DeliveryBL.cs
@@ -0,0 +1,96 @@
+﻿using DataAccessLayer.ModuleOrder;
+using NaviCommon;
+using ObjInfo.ModuleOrders;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessFacadeLayer.ModuleOrders
+{
+    public class DeliveryBL
+    {
+        public List<DeliveryInfo> GetAll_Delivery()
+        {
+            DeliveryDA Obj_da = new DeliveryDA();
+            List<DeliveryInfo> List_Obj = new List<DeliveryInfo>();
+            try
+            {
+                var ds = Obj_da.GetAll_Delivery();
+                List_Obj = CBO<DeliveryInfo>.FillCollectionFromDataSet(ds);
+            }
+            catch (Exception ex)
+            {
+                Common.log.Error(ex.ToString());
+            }
+            return List_Obj;
+        }
+        public DeliveryInfo GetDeliveryByID(decimal delivery_id)
+        {
+            DeliveryDA Obj_da = new DeliveryDA();
+            DeliveryInfo Obj_Info = new DeliveryInfo();
+            try
+            {
+                var ds = Obj_da.GetDeliveryByID(delivery_id);
+                Obj_Info = CBO<DeliveryInfo>.FillObject_From_DataSet(ds);
+            }
+            catch (Exception ex)
+            {
+                Common.log.Error(ex.ToString());
+            }
+            return Obj_Info;
+        }
+        public List<DeliveryInfo> GetDeliveryByOrder(string Order_code)
+        {
+            DeliveryDA Obj_da = new DeliveryDA();
+            List<DeliveryInfo> List_Obj = new List<DeliveryInfo>();
+            try
+            {
+                var ds = Obj_da.GetDeliveryByOrder(Order_code);
+                List_Obj = CBO<DeliveryInfo>.FillCollectionFromDataSet(ds);
+            }
+            catch (Exception ex)
+            {
+                Common.log.Error(ex.ToString());
+            }
+            return List_Obj;
+        }
+        public decimal Inser_Delivery(DeliveryInfo Obj_Info)
+        {
+            DeliveryDA Obj_da = new DeliveryDA();
+            try
+            {
+                return Obj_da.InsertDelivery(Obj_Info);
+            }
+            catch (Exception ex)
+            {
+                Common.log.Error(ex.ToString());
+                return -1;
+            }
+        }
+        public decimal Update_Delivery(DeliveryInfo Obj_Info)
+        {
+            DeliveryDA Obj_da = new DeliveryDA();
+            try
+            {
+                return Obj_da.UpdateDelivery(Obj_Info);
+            }
+            catch (Exception ex)
+            {
+                Common.log.Error(ex.ToString());
+                return -1;
+            }
+        }
+        public decimal Delete_Delivery(decimal delivery_id)
+        {
+            DeliveryDA Obj_da = new DeliveryDA();
+            try
+            {
+                return Obj_da.DeleteDelivery(delivery_id);
+            }
+            catch (Exception ex)
+            {
+                Common.log.Error(ex.ToString());
+                return -1;
+            }
+        }
+    }
+}
---
+++ WIP/Program/ObjInfo/ModuleOrders/DeliveryInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjInfo.ModuleOrders
+{
+    public class DeliveryInfo
+    {
+        public decimal STT { get; set; }
+        public decimal ID { get; set; }
+        /// <summary>
+        /// 1: Bán hàng tại cảng
+        /// 2: Kéo về kho
+        /// </summary>
+        public decimal Delivery_Type { get; set; }
+        public string Delivery_Type_Display { get; set; }
+        public string Order_Code { get; set; }
+        /// <summary>
+        /// Danh sách container được chọn, cách nhau bằng dấu ,
+        /// </summary>
+        public string LST_CONT { get; set; }
+        /// <summary>
+        /// 0:chờ kéo
+        /// 1:Đã kéo
+        /// </summary>
+        public decimal Status { get; set; }
+        public string Status_Display { get; set; }
+        public DateTime Delivery_Date { get; set; }
+        public string Createdby { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifyBy { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public decimal Deleted { get; set; }
+    }
+}
