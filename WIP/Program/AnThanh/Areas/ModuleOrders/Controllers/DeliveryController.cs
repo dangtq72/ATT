4744d9881ee93d1ec937286cbc0e96682974cce2
anhtuan nguyen <nguyenanhtuanxx1996@outlook.com>
2018-11-10 14:44:54 +0700

    update tuanna

---
+++ WIP/Program/AnThanh/Areas/ModuleOrders/Controllers/DeliveryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnThanh.Areas.ModuleOrders.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        // GET: ModuleOrders/Delivery
+        public ActionResult Delivery_Display()
+        {
+            return View();
+        }
+    }
+}
---
+++ WIP/Program/AnThanh/Areas/ModuleOrders/ModuleOrdersAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AnThanh.Areas.ModuleOrders
+{
+    public class ModuleOrdersAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ModuleOrders";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ModuleOrders_default",
+                "ModuleOrders/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
