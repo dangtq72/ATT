4d04f7ba7306da2589bb3b375380bd146475bb63
Phạm Đông <phamvandong2210@gmail.com>
2018-11-12 10:58:19 +0700

    Dongpv_ADD_PO

---
+++ WIP/Program/ObjInfo/PO/Po_Detail_Info.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjInfo.PO
+{
+    public class Po_Detail_Info
+    {
+        public decimal Id { get; set; }
+        public string Po_Code { get; set; }
+        public string Product_Code { get; set; }
+        public DateTime Due_On { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Per { get; set; }
+        public decimal Amount { get; set; }
+        public string Packaging { get; set; }
+    }
+}
---
+++ WIP/Program/ObjInfo/PO/Po_Header_Info.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjInfo.PO
+{
+    public class Po_Header_Info
+    {
+        public decimal Id { get; set; }
+        public string Po_Code { get; set; }
+        public string Pr_Code { get; set; }
+        public string Po_No { get; set; }
+        public string Supplier_Code { get; set; }
+        public DateTime Po_Date { get; set; }
+        public string Price_Type { get; set; }
+        public string Port_Out { get; set; }
+        public string Payment_Type { get; set; }
+        public DateTime Lsd { get; set; }
+        public decimal Cont_Type { get; set; }
+        public decimal Pack_Type { get; set; }
+        public string Packing_Detail { get; set; }
+        public decimal Total_Amount { get; set; }
+        public decimal Status { get; set; }
+        public string Reject_Reason { get; set; }
+        public string Notes { get; set; }
+        public string Created_By { get; set; }
+        public DateTime Created_Date { get; set; }
+        public string Modify_By { get; set; }
+        public DateTime Modify_Date { get; set; }
+        public decimal Deleted { get; set; }
+    }
+}
