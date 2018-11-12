7956f5d23e1c95118d0391b743ad761f0cecbba3
thangnt1202 <nguyentruongthang.ntt@gmail.com>
2018-11-12 10:27:20 +0700

    thangnt

---
+++ WIP/Program/ObjInfo/ModuleOrders/Shipment_Container_Info.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjInfo.ModuleOrders
+{
+    public class Shipment_Container_Info
+    {
+        public decimal Id { get; set; }
+        public string Contract_Code { get; set; }
+        public string Product_Code { get; set; }
+        public string Cont_Code { get; set; }
+        public string Seal_No { get; set; }
+        public decimal Status { get; set; }
+    }
+}
