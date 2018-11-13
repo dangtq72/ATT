3bba856800bfce0b82587136eac2cbe1b4681cde
Phạm Đông <phamvandong2210@gmail.com>
2018-11-13 09:00:48 +0700

    add enum dônpv

---
+++ WIP/Program/NaviCommon/ATT_Enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaviCommon
+{
+   public class ATT_Enum
+    {
+        public enum Port_Type
+        {
+            CangNhap = 1,
+            CangXuat = 2
+        }
+    }
+}
